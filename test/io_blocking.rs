f478262b9a56dcae20a9a50a7a625b6dbac9f3ee
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-01-31 18:41:40 +0100

    Don't crash tests on Windows
    Some strange things happen on Windows. This prevents the (unrelated)
    test from failing, and it is being investigated separately.

--- test/io_blocking.rs
+++ test/io_blocking.rs
@@ -1,35 +1,35 @@
 //! This tests the pretention of blocking IO (Read/Write).
 
 use std::io::{BufRead, BufReader, Write};
+use std::net::{SocketAddr, TcpListener};
+use std::thread;
 
 use corona::prelude::*;
 use corona::io::BlockingWrapper;
-use tokio_core::net::{TcpListener, TcpStream};
+use tokio_core::net::TcpStream;
 use tokio_core::reactor::{Core, Handle};
-use tokio_io::AsyncRead;
 
-const ADDR: &str = "127.0.0.1:2345";
-
-fn server(handle: Handle) {
-    let listener = TcpListener::bind(&ADDR.parse().unwrap(), &handle).unwrap();
-    Coroutine::with_defaults(handle.clone(), move || {
-        for (connection, _address) in listener.incoming().iter_ok() {
-            Coroutine::with_defaults(handle.clone(), move || {
-                let (read, write) = connection.split();
-                let read = BufReader::new(BlockingWrapper::new(read));
-                let mut write = BlockingWrapper::new(write);
-                for line in read.lines() {
-                    let mut line = line.unwrap();
-                    line.push('\n');
-                    write.write_all(line.as_bytes()).unwrap();
+fn server() -> SocketAddr {
+    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let addr = listener.local_addr().unwrap();
+    thread::spawn(move || {
+        while let Ok((connection, _address)) = listener.accept() {
+            let mut connection = BufReader::new(connection);
+            let mut line = String::new();
+            while let Ok(size) = connection.read_line(&mut line) {
+                if size == 0 {
+                    break;
                 }
-            });
+                connection.get_mut().write_all(line.as_bytes()).unwrap();
+                line.clear();
+            }
         }
     });
+    addr
 }
 
-fn client(handle: &Handle) {
-    let connection = TcpStream::connect(&ADDR.parse().unwrap(), handle)
+fn client(addr: &SocketAddr, handle: &Handle) {
+    let connection = TcpStream::connect(addr, handle)
         .coro_wait()
         .unwrap();
     let mut connection = BlockingWrapper::new(connection);
@@ -44,9 +44,9 @@ fn client(handle: &Handle) {
 fn line_req_resp() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    server(handle.clone());
+    let addr = server();
     let done = Coroutine::with_defaults(handle.clone(), move || {
-        client(&handle);
-    });
+            client(&addr, &handle);
+        });
     core.run(done).unwrap();
 }
