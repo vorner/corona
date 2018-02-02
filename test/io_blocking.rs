e0b344aea9d35965aefd976a95065014659c9cc4
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-02-02 18:13:11 +0100

    Re-enable the server part of io wrapper test
    After getting the windows fix from context, go back from threads to
    coroutines in the server part of the test.

--- test/io_blocking.rs
+++ test/io_blocking.rs
@@ -1,28 +1,30 @@
 //! This tests the pretention of blocking IO (Read/Write).
 
 use std::io::{BufRead, BufReader, Write};
-use std::net::{SocketAddr, TcpListener};
-use std::thread;
+use std::net::SocketAddr;
 
 use corona::prelude::*;
 use corona::io::BlockingWrapper;
-use tokio_core::net::TcpStream;
+use tokio_core::net::{TcpListener, TcpStream};
 use tokio_core::reactor::{Core, Handle};
+use tokio_io::AsyncRead;
 
-fn server() -> SocketAddr {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+fn server(handle: Handle) -> SocketAddr {
+    let listener = TcpListener::bind(&"127.0.0.1:0".parse().unwrap(), &handle).unwrap();
     let addr = listener.local_addr().unwrap();
-    thread::spawn(move || {
-        while let Ok((connection, _address)) = listener.accept() {
-            let mut connection = BufReader::new(connection);
-            let mut line = String::new();
-            while let Ok(size) = connection.read_line(&mut line) {
-                if size == 0 {
-                    break;
+    Coroutine::with_defaults(handle.clone(), move || {
+        for (connection, _address) in listener.incoming().iter_ok() {
+            Coroutine::with_defaults(handle.clone(), move || {
+                let (read, write) = connection.split();
+                let read = BufReader::new(BlockingWrapper::new(read));
+                let mut write = BlockingWrapper::new(write);
+                for line in read.lines() {
+                    let mut line = line.unwrap();
+                    line.push('\n');
+                    write.write_all(line.as_bytes()).unwrap();
+                    write.flush().unwrap();
                 }
-                connection.get_mut().write_all(line.as_bytes()).unwrap();
-                line.clear();
-            }
+            });
         }
     });
     addr
@@ -40,11 +42,15 @@ fn client(addr: &SocketAddr, handle: &Handle) {
 }
 
 /// Runs both the client and server on the same thread, so we can be sure they can switch.
+///
+/// Both use the „normal“ blocking API, but switch coroutines under the hood.
+///
+/// The test runs a listener, makes a single connection and exchanges a line there and back.
 #[test]
 fn line_req_resp() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    let addr = server();
+    let addr = server(handle.clone());
     let done = Coroutine::with_defaults(handle.clone(), move || {
             client(&addr, &handle);
         });
